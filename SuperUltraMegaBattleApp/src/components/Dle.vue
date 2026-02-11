bc42b59ca0337daa38c9893728afcf276bae6755
rabute231 <antoinerabute@gmail.com>
2026-02-11 17:57:38 +0100

    page commencée

---
+++ SuperUltraMegaBattleApp/src/components/Dle.vue
@@ -0,0 +1,14 @@
+<script>
+    function rechercher(id){
+        fetch('https://superheroapi.com/api/access-token/{{id}}/powerstats',{
+
+        })
+    }
+</script>
+<template>
+    <h2>
+        Trouve le super héros, tu as 5 essais
+    </h2>
+    <input type="search" v-on:change=""/>
+    <button v-on:click="">Deviner</button>
+</template>
