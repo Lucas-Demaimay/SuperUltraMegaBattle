bdae6c04f05ae585922f71e0f1f228617a838e48
rabute231 <antoinerabute@gmail.com>
2026-02-11 17:48:33 +0100

    page d'accueil

---
+++ SuperUltraMegaBattleApp/src/components/Accueil.vue
@@ -0,0 +1,41 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1>SuperHérosDle</h1>
+    <button>START GAME</button>
+    <button>Search</button>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+ position: absolute;
+}
+
+button {
+  opacity: 1;
+  background: #7D0080;
+  text-shadow: 2px 0 0 #000000, -2px 0 0 #000000, 0 2px 0 #000000, 0 -2px 0 #000000, 1px 1px #000000, -1px -1px 0 #000000, 1px -1px 0 #000000, -1px 1px 0 #000000;
+  border: 3px solid #ffffffff;
+  border-radius: 25px;
+  color: white;
+  padding: 10px 10px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 18px;
+  cursor: pointer;
+}
+
+</style>
