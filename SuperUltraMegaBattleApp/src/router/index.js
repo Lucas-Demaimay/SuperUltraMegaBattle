9e3be1c7eccfa9ad233e3a95f0725af6ed75dc48
Lucas Demaimay <demaima231@unicaen.fr>
2026-02-12 13:57:06 +0100

    deployment strategy

--- SuperUltraMegaBattleApp/src/router/index.js
+++ SuperUltraMegaBattleApp/src/router/index.js
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 import AccueilView from "@/views/AccueilView.vue";
 import SearchView from "@/views/SearchView.vue";
 import DleView from "@/views/DleView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
--- SuperUltraMegaBattleApp/vite.config.js
+++ SuperUltraMegaBattleApp/vite.config.js
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
+
+  base: '/superultramegabattle/',
+
   plugins: [
     vue(),
     vueDevTools(),
